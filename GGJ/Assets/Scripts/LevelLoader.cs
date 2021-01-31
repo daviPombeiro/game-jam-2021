b5dabdc5ed5516dc2e2b125b92e3d4b00721f152
Meika Oliveira <meikafoliveira@gmail.com>
2021-01-31 12:51:24 -0300

    Add transition animation

---
+++ GGJ/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public void Play(){
+        StartCoroutine(LoadLevel("GameScene"));
+    }
+
+    public void Credits(){
+        StartCoroutine(LoadLevel("CreditsScene"));
+    }
+    public void Restart() {
+        StartCoroutine(LoadLevel("StartScene"));
+        Score.scoreValue = 0;
+    }
+
+    IEnumerator LoadLevel(string levelName){
+        // Play animatioin
+        transition.SetTrigger("Start");
+
+        // Wait
+        yield return new WaitForSeconds(1f);
+
+        //Load scene
+        SceneManager.LoadScene(levelName);
+
+    }
+}
